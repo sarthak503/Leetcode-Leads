b836b796595325656f1c876dac15ca18dce79e04
Sarthak Kumar <34093585+sarthak503@users.noreply.github.com>
2022-03-08 14:19:03 +0530

    Time: 22 ms (22.92%), Space: 8 MB (58.87%) - LeetHub

--- 141-linked-list-cycle/141-linked-list-cycle.cpp
+++ 141-linked-list-cycle/141-linked-list-cycle.cpp
@@ -9,29 +9,17 @@
 class Solution {
 public:
     bool hasCycle(ListNode *head) {
-        bool flag=false;
-        unordered_map<ListNode *, int> umap;
-        ListNode *ptr1=head;
+        if(head==NULL) return false;
         ListNode *ptr=head;
-        while(ptr!=NULL){
-            if(umap.find(ptr)!=umap.end())
-            {
-                flag=true;
-                break;
-            }
-            else{
-                umap[ptr]=-1;
-                ptr=ptr->next;
-            }
+        ListNode *ptr1=head;
+        while(ptr!=NULL && (ptr->next)!=NULL){
+            ptr=ptr->next->next;
+            ptr1=ptr1->next;
+            
+            if(ptr==ptr1)
+                return true;
         }
-        // ListNode *ptr1=head;
-        // while((ptr1->next)!=NULL){
-        //    if(umap[ptr1]>1)
-        //    {
-        //        flag=true;
-        //        break;
-        //    }
-        // }       
-    return flag;
+            
+    return false;
     }
 };
