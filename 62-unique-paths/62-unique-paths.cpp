0876b276732eb70dd23a79c69ee5be1037eac23f
Sarthak Kumar <34093585+sarthak503@users.noreply.github.com>
2022-08-01 22:26:26 +0530

    Time: 0 ms (100.00%), Space: 5.8 MB (98.48%) - LeetHub

---
+++ 62-unique-paths/62-unique-paths.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int uniquePaths(int m, int n) {
+          
+        int nn = m+n-2;
+        int rr = min(m-1,n-1);
+        double ans = 1;
+        
+        for(double i=1;i<=rr;i++){
+            ans *=(nn--)/i;
+        }
+        
+        if(ans > int(ans)+0.5){
+            return ceil(ans);
+        }
+       return int(ans);
+    }
+};
