dcf21ee9c6226a962041c45cd562a1931223430c
Sarthak Kumar <34093585+sarthak503@users.noreply.github.com>
2022-08-11 13:51:35 +0530

    Time: 12 ms (89.87%), Space: 12.5 MB (71.91%) - LeetHub

---
+++ 692-top-k-frequent-words/692-top-k-frequent-words.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    #define psi pair<int, string>
+    class comp {
+    public:
+    bool operator() (const psi &p1, const psi &p2) {
+      if(p1.first == p2.first) return p1.second < p2.second;
+      
+      return p1.first > p2.first;
+    }
+    };
+    vector<string> topKFrequent(vector<string>& words, int k) {
+        priority_queue<pair<int,string>,vector<pair<int,string>>,comp> pq;
+        unordered_map<string,int> m;
+        for(int i=0;i<words.size();i++){
+            m[words[i]]++;
+        }
+        for(auto x:m){
+            pq.push({x.second,x.first});
+            if(pq.size()>k)
+                pq.pop();
+        }
+          vector<string> ans(k);
+          int p = k-1;
+
+          while(pq.size() > 0) {
+            ans[p--] = pq.top().second;
+            pq.pop();
+          }
+
+          return ans;
+    }
+};
