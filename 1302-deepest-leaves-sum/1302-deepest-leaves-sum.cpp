980bafa066ba1d9a7c47a48a5b2f6797e685bcf4
Sarthak Kumar <34093585+sarthak503@users.noreply.github.com>
2022-08-22 14:21:45 +0530

    Time: 154 ms (57.71%), Space: 59.7 MB (93.76%) - LeetHub

--- 1302-deepest-leaves-sum/1302-deepest-leaves-sum.cpp
+++ 1302-deepest-leaves-sum/1302-deepest-leaves-sum.cpp
@@ -11,23 +11,41 @@
  */
 class Solution {
 public:
-    
-    int deepestLeavesSum(TreeNode* root) {
-        queue<TreeNode*> q;
-        q.push(root);
-        int ans=0;
-        while(!q.empty()){
-            int si=q.size();
-            int s=0;
-            while(si--){
-                TreeNode* h=q.front();
-                q.pop();
-                s+=h->val;
-                if(h->left) q.push(h->left);
-                if(h->right) q.push(h->right);
+    void dfs(TreeNode* root, int d,int &dpt,int &s){
+        if(!root) return ;
+        if(!root->left && !root->right){
+            if(d==dpt){
+                s+=root->val;
             }
-            if(q.empty())ans=s;
+            else if(d>dpt){
+                dpt=d;
+                s=root->val;
+            }
+            return ;
         }
-        return ans;
+          dfs(root->left,d+1,dpt,s);
+          dfs(root->right,d+1,dpt,s);
+    }
+    int deepestLeavesSum(TreeNode* root) {
+        //BFS 
+        //queue<TreeNode*> q;
+        // q.push(root);
+        // int ans=0;
+        // while(!q.empty()){
+        //     int si=q.size();
+        //     int s=0;
+        //     while(si--){
+        //         TreeNode* h=q.front();
+        //         q.pop();
+        //         s+=h->val;
+        //         if(h->left) q.push(h->left);
+        //         if(h->right) q.push(h->right);
+        //     }
+        //     if(q.empty())ans=s;
+        // }
+        // return ans;
+        int s=0,maxdpt=1;
+        dfs(root,1,maxdpt,s);
+        return s;
     }
 };
