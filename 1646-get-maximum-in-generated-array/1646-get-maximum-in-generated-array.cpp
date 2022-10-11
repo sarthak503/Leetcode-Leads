9e48ad31c6b6e90e1f0b9cb748439ca2e2f6e090
Sarthak Kumar <34093585+sarthak503@users.noreply.github.com>
2022-10-11 21:24:11 +0530

    Time: 2 ms (47.97%), Space: 6.3 MB (54.58%) - LeetHub

--- 1646-get-maximum-in-generated-array/1646-get-maximum-in-generated-array.cpp
+++ 1646-get-maximum-in-generated-array/1646-get-maximum-in-generated-array.cpp
@@ -5,21 +5,21 @@ public:
         // nums[even=i]=nums[i/2]
         // nums[2*i+1]=nums[i]+nums[i+1]
         // nums[odd=i]=nums[i/2]+nums[(i/2)+1]
-        if(n==0) return 0;
-        if(n==1) return 1;
+        if(n<2) return n;
         vector<int> nums;
         nums.push_back(0);
         nums.push_back(1);
+        
+        int mx=INT_MIN;
         for(int i=2;i<=n;i++){
             if(i%2==0)
                 nums.push_back(nums[i/2]);
             else
                 nums.push_back(nums[i/2]+nums[(i/2)+1]);
-                
+            mx=max(mx,nums[i]);
         }
-        int mx=INT_MIN;
-        for(auto x:nums)
-            mx=max(mx,x);
+        
+            
         
         return mx;
             
