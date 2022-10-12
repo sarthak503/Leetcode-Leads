51c1497e55c4baa7ef22d88be90b05126e2a9806
Sarthak Kumar <34093585+sarthak503@users.noreply.github.com>
2022-10-12 19:43:21 +0530

    Time: 125 ms (5.51%), Space: 21.9 MB (71.51%) - LeetHub

---
+++ 0976-largest-perimeter-triangle/0976-largest-perimeter-triangle.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int largestPerimeter(vector<int>& nums) {
+        //All you have to do is use the Triangle Inequality Theorem, 
+        //which states that the sum of two side lengths of a triangle
+        //is always greater than the third side.
+        int mx=INT_MIN;
+        sort(nums.begin(),nums.end());
+        for(int i=0;i<nums.size()-2;i++)
+        if(nums[i]+nums[i+1]>nums[i+2] && nums[i]+nums[i+2]>nums[i+1] & nums[i+1]+nums[i+2]>nums[0]){
+            mx=max(mx,nums[i]+nums[i+1]+nums[i+2]);
+        }
+        if(mx==INT_MIN)
+        return 0;
+        return mx;
+    }
+};
